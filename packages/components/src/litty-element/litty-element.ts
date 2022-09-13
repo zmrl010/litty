e54a96b8a334c937b1cf43a32e7b7b7ec746a899
Zach <zmriel@gmail.com>
2022-09-13 10:50:30 -0400

    feat(components): base element derived from LitElement

---
+++ packages/components/src/litty-element/litty-element.ts
@@ -0,0 +1,8 @@
+import { LitElement } from "lit";
+
+/**
+ * Base element that is just a light wrapper around `LitElement`
+ *
+ * @see {@link LitElement}
+ */
+export class LittyElement extends LitElement {}
