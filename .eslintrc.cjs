58e8e9b799ab61677fa74254bbf41f338772f848
Zach <zmriel@gmail.com>
2022-09-16 10:55:12 -0400

    chore: use more robust tsconfig pattern

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -5,7 +5,7 @@ module.exports = {
   root: true,
   extends: ["@zmrl", "@zmrl/eslint-config/lit"],
   parserOptions: {
-    project: "./tsconfig.lint.json",
+    project: ["./tsconfig.lint.json", "./packages/*/tsconfig.json"],
     sourceType: "module",
   },
   ignorePatterns: ["dist", "node_modules"],
